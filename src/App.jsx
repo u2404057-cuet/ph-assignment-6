5b8ea746ea535e14511da613f25aad327610a00e
u2404057-cuet <u2404057@student.cuet.ac.bd>
2026-03-30 08:28:34 +0600

    packages installed and added a navbar

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { ToastContainer } from 'react-toastify'
+import DaisyNav from './components/daisyNav/DaisyNav'
+
+function App() {
+
+  return (
+    <>
+      <DaisyNav></DaisyNav>
+      <ToastContainer />
+    </>
+  )
+}
+
+export default App
